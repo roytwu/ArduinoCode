247ef93f395628dd7e06bf68eb6c8ca0bc53a2c5
Roy (MSI) <roywu2018@gmail.com>
2020-02-29 18:33:51 -0500

    adding new script avbout array

---
+++ array/array.ino
@@ -0,0 +1,37 @@
+int arr[10];
+
+
+void setup() 
+{
+    //* open the serial port at 9600 bps:
+    Serial.begin(9600);
+}
+
+void loop() 
+{
+  int len = sizeof(arr)/sizeof(arr[0]);
+  Serial.print("\nlegnth of Array is...") ;
+  Serial.print(len) ;  
+  for (int i=0; i<len; i++ )
+  {
+    arr[i] = i*10; 
+  }  
+  
+  Serial.print("\nPrint array data...\n") ; 
+  printArray(arr, len);
+
+  Serial.print("\n\n\n\n\n");
+  int sec=3;
+  delay(sec*1000);
+}
+
+
+void printArray(int n[], int len)
+{
+   for ( int i = 0; i < len; ++i )
+   {
+      int output = n[i];
+      Serial.print(output) ;
+      Serial.print("\n") ;
+   }   
+}
--- basic/basic.ino
+++ basic/basic.ino
@@ -1,19 +1,19 @@
 //* Demonstrating Modulo Arithmetic 
 //* with Arduino UNO basic data types 
 
-#define BYTE
-
-#ifdef BYTE
- byte var = pow(2, 8)-3; //* 1 byte
-#endif
-
-#ifdef INT
- int var = pow(2, 15)-1;  //* 2 byte
-#endif 
-
-#ifdef UINT
- unsigned int var = pow(2, 16)-2; 
-#endif
+//#define BYTE
+//
+//#ifdef BYTE
+// byte var = pow(2, 8)-3; //* 1 byte
+//#endif
+//
+//#ifdef INT
+// int var = pow(2, 15)-1;  //* 2 byte
+//#endif 
+//
+//#ifdef UINT
+// unsigned int var = pow(2, 16)-2; 
+//#endif
 
 
 void setup() 
@@ -26,21 +26,17 @@ void loop() {
   Serial.print("\n---------------------");
   Serial.print("\nSerial Comm starts...\n");
   Serial.print("---------------------\n");
-//  Serial.print(too, BIN); //* binary
-//  Serial.println("\t");
-//  Serial.print(too, DEC); //* decimal
-//  Serial.println("\t");
-//  Serial.print(too, HEX); //* hexadecimal
-//  Serial.println("\t");
 
-  printVar();
+  byte var = pow(2, 8)-3;
+  //int var  = pow(2, 15)-1;
+  printVar(var);
 
   int sec = 4;
   delay(sec*1000); //* delay in milliseconds
   //while(1){}   //* infinity loop
 }
 
-void printVar()
+void printVar(size_t var)
 {
   int sizeVar = sizeof(var);
   Serial.print("size of var is... ");
