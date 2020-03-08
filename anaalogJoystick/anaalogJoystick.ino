c6dfb845e5eb64cf060d6832498b7e81f54246c1
Roy (MSI) <roywu2018@gmail.com>
2020-03-08 13:31:40 -0400

    adding comments

--- anaalogJoystick/anaalogJoystick.ino
+++ anaalogJoystick/anaalogJoystick.ino
@@ -1,32 +1,32 @@
-//* ****************************
+//* ****************************************
 //*　Author:      Roy Wu
 //*　Description: Read analog joystick
-//* ****************************
+//* ****************************************
 
 //* Arduino pin numbers
 const int switchPin = 11; //* digital pin connected to switch output
 const int xPin = A4;      //* analog pin connected to X output
 const int yPin = A5;      //* analog pin connected to Y output
 
+//* ---------------------------------------
 void setup() 
 {
   pinMode(switchPin, INPUT);
   digitalWrite(switchPin, HIGH);
   Serial.begin(9600);
-}
+} 
+//* ---------------------------------------
 
+
+//* ---------------------------------------
 void loop() 
 {
-  int xValue = analogRead(xPin); //* 0-1024
-  int yValue = analogRead(yPin); //* 0-1024
+  int xValue = analogRead(xPin); //* 0 - 1024
+  int yValue = analogRead(yPin); //* 0 - 1024
   int switchV = digitalRead(switchPin);//* high & low
   
-  //Serial.print("Switch:  ");
-  //Serial.print(switchV);
-  //Serial.print("\n");
-  
-  //Serial.print("X-axis: ");
-  //Serial.println(analogRead(X_pin));
+  //* print switch value
+  //Serial.println(switchV);
   //Serial.print("\n");
 
   //* print X&Y separately
@@ -41,3 +41,4 @@ void loop()
   int sec = 1.0;
   delay(sec*100);
 }
+//* ---------------------------------------
