d2ca075f0cdc6cc98a395dad554b375a8b54bdc4
Roy (MSI) <roywu2018@gmail.com>
2020-03-08 17:36:24 -0400

    adding analogWrite example

--- bitBanging/bitBanging.ino
+++ bitBanging/bitBanging.ino
@@ -15,19 +15,35 @@ void setup()
 //* ------------------------------
 void loop() 
 {  
+  //* ----- ----- ----- -----
   //* Beeping every 0.5 sec
+  //* ----- ----- ----- -----
 //  digitalWrite(buzzPin, HIGH); //* buzzer on!!!
 //  delay(0.5*1000);             //* wait
 //  digitalWrite(buzzPin, LOW);  //* buzzer off..
 //  delay(0.5*1000);             //* wait  
 
+
+  //* ----- ----- ----- -----
+  //* adjusting volume with analogWrite
+  //* only every digital pins can do this
+  //* ----- ----- ----- -----
+//  analogWrite(buzzPin, 30); //* 0 - 255
+//  delay(0.5*1000);
+//  analogWrite(buzzPin, 10); //* 0 - 255
+//  delay(0.5*1000);
+
+
+  //* ----- ----- ----- -----
   //* bit banging
-  int dc=300; //* duty cycle
+  //* you can ban all the digital pins
+  //* Approximately dc/10% duty cycle @ 1KHz
+  //* ----- ----- ----- -----  
+  int dc=800; //* duty cycle
   digitalWrite(buzzPin, HIGH);
-  delayMicroseconds(dc);     //* Approximately 10% duty cycle @ 1KHz  
+  delayMicroseconds(dc);     
   digitalWrite(buzzPin, LOW);
-  delayMicroseconds(1000 - dc);
-//  delay(500);
+  delayMicroseconds(2000 - dc);
   
 
 //  int sec =0.5;
