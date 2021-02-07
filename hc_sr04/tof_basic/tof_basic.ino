f1643732df72a39c78a4f536f9daddfdd7b240df
Roy <roywu2018@gmail.com>
2021-02-07 12:57:37 -0500

    renaming files

--- hc_sr04/tof_basic/tof_basic.ino
+++ hc_sr04/tof_basic/tof_basic.ino
@@ -1,25 +1,32 @@
-//* ****************************************
+//* ===== ===== ===== ===== =====
 //* Author:      Roy Wu
-//* Description: Basic program for Ultrasonic Distance Sensor (HC-SR04)
-//* ****************************************
+//* Description: Testing script program for Ultrasonic Distance Sensor (HC-SR04)
+//* History:     -??/??/2020, initial version
+//*              -02/07/2021, project revivial, minor update  
+//* ===== ===== ===== ===== =====
 #include <SR04.h>    //* 3rd party library
 
-#define TRIG_PIN 12  //* UNO output
-#define ECHO_PIN 11  //* UNO input
+#define TRIG_PIN 7  //* UNO output
+#define ECHO_PIN 6  //* UNO input
+
 SR04 oUS = SR04(ECHO_PIN, TRIG_PIN);
 long dis;
 long avgDis;
 
-//* ------------------------------
+//* ---------- ----------
+//*     standard Arduino setup
+//* ---------- ----------
 void setup() 
 {
    Serial.begin(9600);
    delay(1000);
 }
-//* ------------------------------
 
 
-//* ------------------------------
+
+//* ---------- ----------
+//*     standard Arduino loop
+//* ---------- ----------
 void loop() 
 {
    dis = oUS.Distance();
@@ -34,4 +41,3 @@ void loop()
    //Serial.println("\n\n\n\n");
    //delay(1000);
 }
-//* ------------------------------
