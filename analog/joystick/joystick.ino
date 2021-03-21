8c0ec6b02b15afd8e31bc7220c74e76b0d72b5e7
Roy (MSI) <roywu2018@gmail.com>
2021-03-21 12:44:21 -0400

    adding buzzOnOff() function

--- analog/joystick/joystick.ino
+++ analog/joystick/joystick.ino
@@ -1,21 +1,55 @@
 //* ===== ===== ===== ===== =====
 //* Author:      Roy Wu
-//* Description: Read analog joystick
+//* Description: Analog joystick with an active buzzer
+//*              Buzzer tone can be adjusted by chaning joystick's position
+//*              Buzzer noise can be minimied by pressing the push button on the joystick
 //* History:     -05/xx/2020 initial version
+//*              -03/21/2021 adding buzzOnOff()
 //* ===== ===== ===== ===== =====
 
+const int switchPin = 2; //* joystick  
+const int xPin = A0;     //* joystick
+const int yPin = A1;     //* joystick
+int buzzPin = 8;         //* buzzer
 
-const int switchPin = 2;  //* switch output
-const int xPin = A0;      //* X output
-const int yPin = A1;      //* Y output
+
+//* toggle viarable for buzzer's pinMode
+int pinModeSW = 0;  //* 0: output, 1:input
 
 
+//* ---------- 
+//*     turn buzzer on/off by changing the pinMode
+//* ---------- 
+void buzzOnOff()
+{
+  switch (pinModeSW)
+  {
+    case 0:
+      //* current pMode is OUTPUT, swith to INPUT and then break the loop
+      //* buzzer shall be quite aftewards
+      pinMode(buzzPin, INPUT);
+      pinModeSW=1;
+      Serial.print(" ");
+      Serial.println(pinModeSW);
+      break;
+    case 1:  
+      //* current pMode is INPUT, swith to OUTPUT and break then the loop
+      //* buzzer shall be noisy aftewards
+      pinMode(buzzPin, OUTPUT);
+      pinModeSW=0;
+      Serial.print(" ");
+      Serial.println(pinModeSW);
+      break;
+ }
+}
 
 //* ---------- ---------- ----------
 //*     standard Arduino setup
 //* ---------- ---------- ----------  
 void setup() 
 {
+  pinMode(buzzPin,  OUTPUT);
+  
   pinMode(switchPin, INPUT);
   digitalWrite(switchPin, HIGH);
   
@@ -31,21 +65,36 @@ void loop()
 {
   int xValue = analogRead(xPin); //* 0 to 1024
   int yValue = analogRead(yPin); //* 0 to 1024
-  int switchV = digitalRead(switchPin);//* high & low
+  int switchV = digitalRead(switchPin); //* high or low
   
   //* print switch value
 //  Serial.println(switchV);
 //  Serial.print("\n");
 
-  //* print X&Y separately
-//  Serial.println(xValue);
-//  Serial.print("\n\n");
-
   //* ploting x and Y pin together
-  Serial.print(xValue);
-  Serial.print(" ");
-  Serial.println(yValue);
+//  Serial.print(xValue);
+//  Serial.print(" ");
+//  Serial.println(yValue);
 
-  int sec = 1.0;
-  delay(sec*1000);
+  //* when the push botton is pressed, toggle the buzzer on/off
+  if (switchV ==0)
+  {
+    buzzOnOff();
+  }
+
+  int i;
+  int counter = 0;
+  while(counter < 5)
+  {
+    //* buzzing in one frequency
+    for(i=0; i<80; i++)
+    {
+      digitalWrite(buzzPin ,HIGH);
+      delayMicroseconds(xValue);
+      digitalWrite(buzzPin, LOW);
+      delayMicroseconds(yValue);
+    }
+    counter++;   
+  } 
+  delay(500);
 }
