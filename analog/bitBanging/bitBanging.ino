d46af7113fa54577d351b8fe2b22d53c766a4247
Roy <roywu2018@gmail.com>
2021-03-22 09:25:33 -0400

    cleaning up

--- analog/bitBanging/bitBanging.ino
+++ analog/bitBanging/bitBanging.ino
@@ -1,32 +1,36 @@
-//* ****************************************
+//* ===== ===== ===== ===== =====
 //* Author:      Roy Wu
-//* Description: Bit-banging
-//* ****************************************
+//* Description: bit-banging
+//* History:     -04/xx/2020 initial version
+//* ===== ===== ===== ===== =====
 int buzzPin = 5;
 
-//* ------------------------------
+//* ---------- ---------- ----------
+//*     standard Arduino setup
+//* ---------- ---------- ----------  
 void setup() 
 {
   Serial.begin(9600);       //* open the serial port at 9600 bps:
   pinMode(buzzPin, OUTPUT); //* Declaring buzzer pin as output
 }
-//* ------------------------------
 
-//* ------------------------------
+
+//* ---------- ---------- ----------
+//*     standard Arduino loop
+//* ---------- ---------- ----------  
 void loop() 
 {  
   //* ----- ----- ----- -----
   //* Beeping every 0.5 sec
   //* ----- ----- ----- -----
-//  digitalWrite(buzzPin, HIGH); //* buzzer on!!!
-//  delay(0.5*1000);             //* wait
-//  digitalWrite(buzzPin, LOW);  //* buzzer off..
-//  delay(0.5*1000);             //* wait  
-
+//  digitalWrite(buzzPin, HIGH); 
+//  delay(0.5*1000);             
+//  digitalWrite(buzzPin, LOW);  
+//  delay(0.5*1000);             
 
   //* ----- ----- ----- -----
   //* adjusting volume with analogWrite
-  //* only every digital pins can do this
+  //* pin 3, 5, 6, 9, 10, 11
   //* ----- ----- ----- -----
 //  analogWrite(buzzPin, 150); //* 0 - 255
 //  delay(0.5*1000);
@@ -36,7 +40,7 @@ void loop()
 
   //* ----- ----- ----- -----
   //* bit banging
-  //* you can ban all the digital pins
+  //* one can bang all the digital pins
   //* Approximately dc/10% duty cycle @ 1KHz
   //* ----- ----- ----- -----  
   int dc=100; //* duty cycle
@@ -49,4 +53,3 @@ void loop()
 //  int sec =0.5;
 //  delay(sec*1000);
 }
-//* ------------------------------
