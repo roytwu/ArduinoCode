76547978101d905c5a6d091c27cce16499365e5e
Roy (MSI) <roywu2018@gmail.com>
2021-04-23 15:06:20 -0400

    adding a new sketch

---
+++ servo/servo_noLib/servo_noLib.ino
@@ -0,0 +1,59 @@
+//* ===== ===== ===== ===== =====
+//* Author:      Roy Wu
+//* Description: basic program for servo motor
+//* History:     -04/21/2021 initial version
+//* ===== ===== ===== ===== =====
+int buzzPin = 7;
+
+
+//* ---------- ---------- ----------
+//*     standard Arduino setup
+//* ---------- ---------- ----------  
+void setup()
+{
+  pinMode(buzzPin, OUTPUT);
+}
+
+
+//* ---------- ---------- ----------
+//*     standard Arduino loop
+//* ---------- ---------- ----------  
+void loop()
+{
+ int i;
+ int counter = 0;
+ while(counter < 3)
+ {
+//   //* buzzing in one frequency
+//   for(i=0; i<80; i++)
+//   {
+//    digitalWrite(buzzPin ,HIGH);
+//    delay(1);
+//    digitalWrite(buzzPin, LOW);
+//    delay(1);
+//   }
+    
+   //* buzzing in another frequency
+//   for(i=0; i<100; i++)
+//   {
+//     digitalWrite(buzzPin, HIGH);
+//     delay(100);
+//     digitalWrite(buzzPin, LOW);
+//     delay(100);
+//   }
+   for(i=0; i<100; i++)
+   {
+     analogWrite(buzzPin, 200);
+     delay(100);
+     analogWrite(buzzPin, 0);
+     delay(100);
+   }
+    
+    
+  counter++;  
+  }
+
+  //* after buzzing 3 cycles, take a 2.5 sec break
+  digitalWrite(buzzPin, LOW);
+  delay(2500);
+} 
